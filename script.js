08390dfa56723c875061b386d8f6ab03d9f80b6d
Michal Pajak <michalpajak96@gmail.com>
2023-09-17 21:01:31 +0200

    resize

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let VIDEO=null;
 let CANVAS=null;
 let CONTEXT=null;
+let SCALER=0.6;
+let SIZE={x:0,y:0,width:0,height:0};
 
 function main() {
     CANVAS= document.getElementById('myCanvas');
     CONTEXT=CANVAS.getContext('2d');
-    CANVAS.width=window.innerWidth;
-    CANVAS.height=window.innerHeight;
 
    let promise=navigator.mediaDevices.getUserMedia({video:true});
    promise.then(function(signal){
@@ -15,6 +15,8 @@ function main() {
     VIDEO.play();
 
     VIDEO.onloadeddata=function () {
+        handleResize();
+        //window.addEventListener('resize', handleResize)
         updateCanvas();
     }
 
@@ -23,7 +25,22 @@ function main() {
    })
 }
 
+function handleResize(){
+    CANVAS.width=window.innerWidth;
+    CANVAS.height=window.innerHeight;
+
+    let resizer=SCALER*
+    Math.min(
+        window.innerWidth/VIDEO.videoWidth,
+        window.innerHeight/VIDEO.videoHeight
+    );
+    SIZE.width=resizer*VIDEO.videoWidth;
+    SIZE.height=resizer*VIDEO.videoHeight;
+    SIZE.x=window.innerWidth/2-SIZE.width/2;
+    SIZE.y=window.innerHeight/2-SIZE.height/2;
+}
+
 function updateCanvas() {
-    CONTEXT.drawImage(VIDEO,0,0);
+    CONTEXT.drawImage(VIDEO, SIZE.x, SIZE.y, SIZE.width, SIZE.height);
     window.requestAnimationFrame(updateCanvas);
 }
